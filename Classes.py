a4aeffae0b0ab479c1f4307cc04aa3a048e1208e
Gustavotfz <gustavo.tfernandez@gmail.com>
2024-05-10 16:53:55 -0300

    pasta classes

---
+++ Classes.py
@@ -0,0 +1,28 @@
+import pygame
+import random
+from constantes import *
+from assets import *
+
+class Icones(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, LARGURA_TELA - ALTURA_LARGURA_ICONES)
+        self.rect.y = random.randint(-100, -ALTURA_LARGURA_ICONES)
+        self.speedx = random.randint(-3, 3)
+        self.speedy = random.randint(2, 9)
+
+    def update(self):
+        # Atualizando a posição do meteoro
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        # Se o meteoro passar do final da tela, volta para cima e sorteia
+        # novas posições e velocidades
+        if self.rect.top > ALTURA_TELA or self.rect.right < 0 or self.rect.left > WIDTH:
+            self.rect.x = random.randint(0, WIDTH-METEOR_WIDTH)
+            self.rect.y = random.randint(-100, -METEOR_HEIGHT)
+            self.speedx = random.randint(-3, 3)
+            self.speedy = random.randint(2, 9)
